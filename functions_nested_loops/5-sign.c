0b49cc39bdbcdbd40ed956d4c55aa671ec36e8db
root <cicekabdullayeva28@gmail.com>
2024-10-27 19:51:36 +0000

    task

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,25 +1,15 @@
 #include "main.h"
 /**
- * print_sign - prints the sign of a number
- * @n: variable
- * Return: if positive 1, zero 0, negative -1
- */
-
-int print_sign(int n)
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
+int _isalpha(int c)
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else
-	{
-		_putchar(45);
-		return (-1);
-	}
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
 }
