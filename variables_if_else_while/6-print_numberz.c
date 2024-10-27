18c53f9da98e7e24954087725b62907ecb51aa11
root <cicekabdullayeva28@gmail.com>
2024-10-27 18:46:01 +0000

    test 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,28 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - main
+ *
+ * Return: zero
  */
 int main(void)
 {
-	int n;
+	int num;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("%d ", n);
-	if (n > 0)
-	{
-		printf("is positive\n");
-	}
-	else if (n == 0)
-	{
-		printf("is zero\n");
-	}
-	else
-	{
-		printf("is negative\n");
-	}
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
 	return (0);
 }
