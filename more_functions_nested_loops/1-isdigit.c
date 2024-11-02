78480052e1c600e3c94e4b2db3fd2487829a0e96
root <cicekabdullayeva28@gmail.com>
2024-11-02 14:25:36 +0000

    task

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-
-int main(void) {
-int c = 0; 
-/* Initialize c to a default value*/
-/* Your code logic here, perhaps updating c based on user input*/
-if (c >= 0 && c <= 9) {
-/* Do something*/
-}
-return 0;
+#include "main.h"
+/**
+ * _isdigit - check digits
+ * @c :checker simvol
+ * Return: if digit 1,else 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
 }
