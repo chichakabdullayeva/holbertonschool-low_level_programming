215fca2f49a72b9e5f972e99a51227745d66ac7d
root <cicekabdullayeva28@gmail.com>
2024-11-02 08:38:21 +0000

    try fix

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-int main (void){
-int c;
-if (c>= 'A' && c>= 'Z') {
+
+int _isupper(int c)
+{
+if (c>= 'A' && c<= 'Z') 
+{
 	return (1);
-	}
-else {
-	return(0);}}
+}
+else 
+{
+	return(0);
+}
+}
