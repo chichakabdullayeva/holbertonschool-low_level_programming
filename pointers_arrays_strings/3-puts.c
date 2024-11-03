3916a4dd5edb073f2f3fecf7909549318d67cbe3
root <cicekabdullayeva28@gmail.com>
2024-11-03 16:02:47 +0000

    task

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,15 +5,13 @@
  * @str: char
  * Return: just a return
  */
-
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (*(str + i) != 0)
-	{
-		_putchar(*(str + i));
-		i++;
-	}
-	_putchar('\n');
+int i = 0;
+while (*(str + i) != 0)
+{
+_putchar(*(str + i));
+i++;
+}
+_putchar('\n');
 }
