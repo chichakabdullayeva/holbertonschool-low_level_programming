f863996321e30dd7db601e0a1ecb755e1535c58b
root <cicekabdullayeva28@gmail.com>
2024-11-03 10:05:10 +0000

    task

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9.
+ *
+ * Description: This function prints the numbers from 0 to 9,
+ * each followed by a space. It does not return any value.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 1; i++)
+{
+printf("%d", i);
+}
+}
