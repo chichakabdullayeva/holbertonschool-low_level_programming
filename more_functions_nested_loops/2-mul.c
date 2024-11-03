93237b5989627c875b4e0d7effc49267a0f9e98f
root <cicekabdullayeva28@gmail.com>
2024-11-03 09:44:39 +0000

    task

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-int mul(int a, int b){
-return a * b;
+int mul(int a, int b)
+{
+return (a * b);
 }
-/*Function to multiply two integers
+/*
+ * Function to multiply two integers
  *Multiply the two integers and store the result
- *Return the result of the multiplication*/
+ *Return the result of the multiplication
+*/
