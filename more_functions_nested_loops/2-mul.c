e5551a6672e9107c43324446375f93ea2bfa5602
root <cicekabdullayeva28@gmail.com>
2024-11-03 09:29:27 +0000

    task

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+int mul(int a, int b){
+return a * b;
+}
+int main (void){
+/*Function to multiply two integers
+ *Multiply the two integers and store the result
+ *Return the result of the multiplication*/
+int c = mul(5, 2);
+printf ("%d",c);}
