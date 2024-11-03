4aec59b5b8757009af07c8ad4e5dcf17d5a6af8e
root <cicekabdullayeva28@gmail.com>
2024-11-03 09:39:06 +0000

    task

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,9 +4,6 @@
 int mul(int a, int b){
 return a * b;
 }
-int main (void){
 /*Function to multiply two integers
  *Multiply the two integers and store the result
  *Return the result of the multiplication*/
-int c = mul(5, 2);
-printf ("%d",c);}
