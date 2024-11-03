655dddee3b630c84a74021c8a1989e4822f4d62b
root <cicekabdullayeva28@gmail.com>
2024-11-03 18:47:51 +0000

    task

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,27 +2,29 @@
 #include <stdio.h>
 
 /**
- * _strchr - locates
- * @s: string
- * @c: char
+ * _strchr - Locates a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
  *
- * Return: char
+ * Return: A pointer to the first occurrence of the character
+ *         in the string, or NULL if the character is not found.
  */
+char *_strncat(char *dest, char *src, int n);
 char *_strchr(char *s, char c)
 {
 int i = 0;
-int j = 0;
-while (*(s + i))
+/* Iterate through the string */
+while (s[i] != '\0')
+/* Check for end of string */
 {
-i++;
-}
-j = i;
-i = 0;
-while (*(s + i) != c && i <= j)
+if (s[i] == c)
+/* Check if the current character matches c */
 {
-i++;
-}
-if (*(s + i) == c)
 return (s + i);
+/* Return pointer to the found character */
+}
+i++;
+}
 return (NULL);
+/* Character not found, return NULL */
 }
