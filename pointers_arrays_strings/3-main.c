fc4b0287b61f95d37562711d48afc0afa26604bc
root <cicekabdullayeva28@gmail.com>
2024-11-14 10:24:24 +0000

    done

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,24 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
+ * main - check the code
  *
- * Return: An integer less than, equal to, or greater than zero if
- *         s1 is found, respectively, to be less than, to match, or
- *         be greater than s2.
+ * Return: Always 0.
  */
-int _strcmp(char *s1, char *s2)
+int main(void)
 {
-while (*s1 && *s2)  /* Compare characters of both strings */
-{
-if (*s1 != *s2)  /* If characters differ, return the difference */
-{
-return (*s1 - *s2);
-}
-s1++;  /* Move to the next character in s1 */
-s2++;  /* Move to the next character in s2 */
-}
-/* If we reach here, at least one string has ended */
-return (*s1 - *s2);  /* Return the difference of the terminating characters */
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+
