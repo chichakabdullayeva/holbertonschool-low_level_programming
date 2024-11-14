669c1776fc6b25829b436a239ddf448740b43473
root <cicekabdullayeva28@gmail.com>
2024-11-14 11:45:29 +0000

    done

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,20 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _puts_recursion - puts
+ * _puts_recursion - check the code
  * @s: string
- *
- * Return: no return
+ * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+else
 {
 _putchar(*s);
 s++;
 _puts_recursion(s);
-}
-else if (*s == 0)
-{
-_putchar('\n');
 }
 }
