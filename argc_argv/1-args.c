e35aacae2a3bfd8744de6e54fe49701a25a6c81e
root <cicekabdullayeva28@.com>
2024-11-16 11:11:47 +0000

    done

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,15 +5,13 @@
  * main - Entry point of the program
  * @argc: Number of command line arguments
  * @argv: Array of strings representing command line arguments
- * 
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 (void) argv;
-    /* Print the number of command line arguments */
-    printf("%d\n", argc - 1);
-
-    return (0);
+/* Print the number of command line arguments */
+printf("%d\n", argc - 1);
+return (0);
 }
 
