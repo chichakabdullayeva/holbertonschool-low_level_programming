b560f6176df0e45637d3243fc52154cbc2bb7899
root <cicekabdullayeva28@.com>
2024-11-16 11:53:08 +0000

    done

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-#include "main.h"
-#include <stdlib.h>
-int main (int argc, char *argv [])
-{
+#include <stdlib.h>  // For atoi()
+
 /**
- * main - check the code
- * @argc: argc
- * @argv: argv
- * Return: Always 0.
+ * main - Multiplies two numbers passed as command line arguments
+ * @argc: The number of command line arguments
+ * @argv: Array of strings representing command line arguments
+ * 
+ * Return: 0 if successful, 1 if there are less than 2 arguments
  */
+int main(int argc, char *argv[])
+{
+/* Check if there are at least two arguments */
 if (argc > 2)
 {
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));  // Multiply arguments and print result
 }
 else
 {
-printf("Error\n");
-return (1);
+printf("Error\n");  // Print error message if not enough arguments
+return (1);  // Return 1 to indicate an error
 }
-return (0);
+return (0);  // Return 0 to indicate success
 }
